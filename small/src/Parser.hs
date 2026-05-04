7b5750f57b54c03225ac820de0d746c46e8e3877
Sergei Winitzki <winitzki@gmail.com>
2026-05-04 23:27:24 +0200

    add PArser.hs

---
+++ small/src/Parser.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE TypeFamilies #-} 
+
+module Parser where
+
+import Lexer (SToken(..), BareToken(..), AlexPosn (..), scanTokens)
+import qualified Data.Vector as V
+import Data.Vector (Vector)
+import Data.Bifunctor (second, bimap)
+import Data.Proxy (Proxy(..))
+-- import Text.Megaparsec.Stream (Stream)
+import Text.Megaparsec (Stream(..), Parsec, parse, ParseErrorBundle)
+
+newtype TokenStream = TokenStream { tokens :: Vector SToken }
+    deriving stock (Eq, Show, Ord)
+
+instance Stream TokenStream where
+
+  type Token TokenStream = SToken
+  type Tokens TokenStream = TokenStream
+
+  take1_ s = second TokenStream <$> V.uncons s.tokens
+  takeN_ n s = if n == 0 then Nothing else Just $ bimap TokenStream TokenStream $ V.splitAt n s.tokens
+  takeWhile_ p s = bimap TokenStream TokenStream $ V.span p s.tokens
+ 
+  tokensToChunk Proxy = TokenStream . V.fromList
+  chunkToTokens Proxy = V.toList . (.tokens)
+  chunkLength Proxy = V.length . (.tokens)
+  chunkEmpty Proxy = V.null . (.tokens)
+
+ 
