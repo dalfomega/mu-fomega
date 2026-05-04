9c18853a52375df1c5d2746c85a21a02ad8c0d38
Sergei Winitzki <winitzki@gmail.com>
2026-05-04 23:27:46 +0200

    reformat

--- small/src/Parser.hs
+++ small/src/Parser.hs
@@ -1,30 +1,28 @@
-{-# LANGUAGE TypeFamilies #-} 
+{-# LANGUAGE TypeFamilies #-}
 
 module Parser where
 
-import Lexer (SToken(..), BareToken(..), AlexPosn (..), scanTokens)
-import qualified Data.Vector as V
+import Data.Bifunctor (bimap, second)
+import Data.Proxy (Proxy (..))
 import Data.Vector (Vector)
-import Data.Bifunctor (second, bimap)
-import Data.Proxy (Proxy(..))
--- import Text.Megaparsec.Stream (Stream)
-import Text.Megaparsec (Stream(..), Parsec, parse, ParseErrorBundle)
+import qualified Data.Vector as V
+import Lexer (AlexPosn (..), BareToken (..), SToken (..), scanTokens)
 
-newtype TokenStream = TokenStream { tokens :: Vector SToken }
+-- import Text.Megaparsec.Stream (Stream)
+import Text.Megaparsec (ParseErrorBundle, Parsec, Stream (..), parse)
+
+newtype TokenStream = TokenStream {tokens :: Vector SToken}
     deriving stock (Eq, Show, Ord)
 
 instance Stream TokenStream where
+    type Token TokenStream = SToken
+    type Tokens TokenStream = TokenStream
 
-  type Token TokenStream = SToken
-  type Tokens TokenStream = TokenStream
+    take1_ s = second TokenStream <$> V.uncons s.tokens
+    takeN_ n s = if n == 0 then Nothing else Just $ bimap TokenStream TokenStream $ V.splitAt n s.tokens
+    takeWhile_ p s = bimap TokenStream TokenStream $ V.span p s.tokens
 
-  take1_ s = second TokenStream <$> V.uncons s.tokens
-  takeN_ n s = if n == 0 then Nothing else Just $ bimap TokenStream TokenStream $ V.splitAt n s.tokens
-  takeWhile_ p s = bimap TokenStream TokenStream $ V.span p s.tokens
- 
-  tokensToChunk Proxy = TokenStream . V.fromList
-  chunkToTokens Proxy = V.toList . (.tokens)
-  chunkLength Proxy = V.length . (.tokens)
-  chunkEmpty Proxy = V.null . (.tokens)
-
- 
+    tokensToChunk Proxy = TokenStream . V.fromList
+    chunkToTokens Proxy = V.toList . (.tokens)
+    chunkLength Proxy = V.length . (.tokens)
+    chunkEmpty Proxy = V.null . (.tokens)
